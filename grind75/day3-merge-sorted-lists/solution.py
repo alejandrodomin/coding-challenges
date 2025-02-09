9b3461f68588377fc7cb283942bbab142376e8d6
A283le <alexdominguez169@gmail.com>
2025-02-08 19:22:41 -0500

    Some documentation

--- grind75/day3-merge-sorted-lists/solution.py
+++ grind75/day3-merge-sorted-lists/solution.py
@@ -1,11 +1,13 @@
 def mergeTwoLists(list1, list2):
     m_list=[]
 
+    # First section: Organize the lists into big and small
     if len(list1) > len(list2): 
         big, small = list1, list2 
     else:
         big, small = list2, list1
 
+    # Second section: Merge elements up to where the small list ends
     for i in range(len(small)):
         if list1[i] > list2[i]:
             first, second = list2[i], list1[i] 
@@ -15,6 +17,7 @@ def mergeTwoLists(list1, list2):
         m_list.append(first)
         m_list.append(second)
 
+    # Third section: The small list is now done, just append the rest of the big list
     for item in big[len(small):]:
         m_list.append(item)
 
