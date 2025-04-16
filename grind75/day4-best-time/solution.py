5e671120a478ec37d3370b899b341cb903a92922
A283le <alexdominguez169@gmail.com>
2025-04-16 10:12:34 -0400

    Proper solution that doesn't have an exponential time complexity.

--- grind75/day4-best-time/solution.py
+++ grind75/day4-best-time/solution.py
@@ -2,14 +2,21 @@ import pdb
 
 def max_profit(prices):
     max_profit=0
-    for index in range(len(prices)):
-        for jindex in range(index, len(prices)):
-            if prices[jindex] - prices[index] > max_profit:
-                max_profit = prices[jindex] - prices[index]
+    buy_ptr, sell_ptr=0,0
+    
+    while sell_ptr < len(prices):
+        delta=prices[sell_ptr] - prices[buy_ptr]
+
+        if delta > max_profit:
+            max_profit = delta
+        elif delta < 0:
+            buy_ptr+=1
+        else:
+            sell_ptr+=1
 
     return max_profit
 
 if __name__=='__main__':
-    prices=[7,6,4,3,1]
+    prices=[7,1,5,3,6,4]
     profit = max_profit(prices)
     print(profit)
