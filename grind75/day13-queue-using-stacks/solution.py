9aea47c320d2c46c786706ddf2dc384ac4da5caf
A283le <alexdominguez169@gmail.com>
2025-04-18 22:39:00 -0400

    Python make this too easy.

---
+++ grind75/day13-queue-using-stacks/solution.py
@@ -0,0 +1,41 @@
+class MyQueue(object):
+    def __init__(self):
+        self.stack=[]
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        if self.stack:
+            return False
+        return True
+
+
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+param_2 = obj.pop()
+param_3 = obj.peek()
+param_4 = obj.empty()
