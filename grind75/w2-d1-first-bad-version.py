37aa5c4b5d04bbb486c60bed03f4c4c21e54e00b
alejandrodomin <alexdominguez169@gmail.com>
2025-04-20 14:01:17 -0400

    The one that broke me. w2-d1-first-bad-version.py
    I don't know what it is about this problem but it's breaking me. I've been trying to crack it for too long I need to move on. Committed from iPhone.

---
+++ grind75/w2-d1-first-bad-version.py
@@ -0,0 +1,31 @@
+def __is_bad_version(n):
+    if n > 39:
+        return True
+    return False
+
+if __name__=="__main__":
+    versions = [None] * 50
+        
+    i = len(versions) // 2
+    for x in range(50):
+        print(i)
+        if versions[i] is None:
+            left = __is_bad_version(i + 1)
+            versions[i] = left
+        if versions[i + 1] is None:
+            right = __is_bad_version(i + 2)
+            versions[i + 1] = right
+            
+        print(i)
+        if left ^ right:
+            print(f"v {i + 1} l {left} r {right}")
+            break
+            
+        if left and right:
+            i -= i // 2
+            i = max(0, i)
+        else:
+            i += i // 2
+            i = min(48, i)
+            
+    print(versions)
