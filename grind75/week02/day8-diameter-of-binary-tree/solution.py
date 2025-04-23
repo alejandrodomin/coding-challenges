1008debf479a07da3b80bd98b8c7fd38ae15aa51
A283le <alexdominguez169@gmail.com>
2025-04-22 23:32:22 -0400

    Ok so leetcode accepts my answer and in order to make it work I had to go over all nodes. But I know for a fact this can't be the best solution. Going to do some research and come back with a proper implementation.

---
+++ grind75/week02/day8-diameter-of-binary-tree/solution.py
@@ -0,0 +1,86 @@
+# my intuition tells me that the 2 nodes with the deepest depth added would
+# give the solution.
+# So depth of root.left + depth of root.right
+
+import heapq
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __lt__(self, other):
+        if other is None:
+            return False
+        return self.val < other.val
+
+
+def gen_tree(values):
+    if not values or values[0] is None:
+        return None
+
+    node = TreeNode(values[0])
+    queue = [node]
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.pop(0)
+
+        # Left child
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        # Right child
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+
+    return node
+
+
+def cache(func):
+    pre_calc = {}
+
+    def cache_value(*args, **kwargs):
+        value = args[0]
+
+        if value not in pre_calc:
+            pre_calc[value] = func(*args, **kwargs)
+
+        return pre_calc[value]
+
+    return cache_value
+
+
+@cache
+def depth(node):
+    if node is None:
+        return -1
+    else:
+        return max(depth(node.left), depth(node.right)) + 1
+
+
+if __name__ == '__main__':
+    root = gen_tree([1, 2, 3, 4, 5])
+    pq = []
+    heapq.heappush(pq, root)
+
+    diameter = 0
+    while pq:
+        node = heapq.heappop(pq)
+
+        if node.left is not None:
+            heapq.heappush(pq, node.left)
+        if node.right is not None:
+            heapq.heappush(pq, node.right)
+
+        local_diam = depth(node.left) + depth(node.right) + 2
+        if local_diam > diameter:
+            diameter = local_diam
+
+    print(diameter)
