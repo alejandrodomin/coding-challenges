90e5709dc7a8f035e191436d0ccb918422d63cc6
A283le <alexdominguez169@gmail.com>
2025-05-02 18:48:49 -0400

    I can't for the life of me figure it out.

--- grind75/week03/day02-01-matrix/solution.py
+++ grind75/week03/day02-01-matrix/solution.py
@@ -1,21 +1,20 @@
 distance_map = {}
 visited = []
 
-def valid_moves(coord: tuple[int, int]):
+def valid_moves(coord: tuple[int, int], matrix: list[list[int]]):
     global visited
 
     row, item = coord
-    # TODO: change these to tuples since they can't be added to the distance map
-    moves = [[row - 1 , item],
-             [row, item - 1],
-             [row, item + 1],
-             [row + 1, item]]
+    moves = [(row - 1 , item),
+             (row, item - 1),
+             (row, item + 1),
+             (row + 1, item)]
 
+    max_r, max_i = len(matrix), len(matrix[0])
     v_moves = []
-    for move in moves:
-        # TODO: check for bounds violations
-        if move not in visited:
-            v_moves.append(move)
+    for x, y in moves:
+        if 0 <= x < max_r and 0 <= y < max_i and (x, y) not in visited:
+            v_moves.append((x, y))
 
     visited.extend(v_moves)
     return v_moves
@@ -24,18 +23,18 @@ def valid_moves(coord: tuple[int, int]):
 def nearest_0(coord, matrix):
     global distance_map
 
-    if coord in distance_map:
-        return distance_map[coord]
+    if coord not in distance_map:
+        if matrix[coord[0]][coord[1]] == 0:
+            distance_map[coord] = 0
+        else:
+            distances = []
+            for move in valid_moves(coord, matrix):
+                distances.append(nearest_0(move, matrix))
 
-    if matrix[coord[0]][coord[1]] == 0:
-        distance_map[coord] = 0
-        return distance_map[coord]
+            distance = min(distances) + 1
+            distance_map[coord] = distance
 
-    distances = []
-    for move in valid_moves(coord):
-        distances.append(nearest_0(move, matrix))
-
-    return min(distances) + 1
+    return distance_map[coord]
 
 
 def distance_matrix(matrix):
