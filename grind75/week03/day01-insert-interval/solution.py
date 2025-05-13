a20bf4f6c86e3fd638766251f993e01a06fe093f
A283le <alexdominguez169@gmail.com>
2025-05-13 00:46:02 -0400

    Improved time complexity from O(n^2) due to list mutations to O(n) by being a little smarter with how I built out the merged array.

--- grind75/week03/day01-insert-interval/solution.py
+++ grind75/week03/day01-insert-interval/solution.py
@@ -1,37 +1,42 @@
 # intuition tells me to use a bit arraend but i dont think that will be necessary
 
-def insert(intervals, new_interval):
+def merge_inter(intervals, new_interval):
     if not intervals:
         return [new_interval]
 
+    __insert(intervals, new_interval)
+    return __merge(intervals)
+
+
+def __merge(intervals):
+    merged_intervals = [intervals[0]]
+    for inter in intervals[1:]:
+        last = merged_intervals.pop()
+
+        if last[1] >= inter[0]:
+            merged = [min(last[0], inter[0]), max(last[1], inter[1])]
+            merged_intervals.append(merged)
+        else:
+            merged_intervals.append(last)
+            merged_intervals.append(inter)
+
+    return merged_intervals
+
+
+def __insert(intervals, new_interval):
     inserted = False
     for index, inter in enumerate(intervals):
         if new_interval[0] <= inter[0]:
             intervals.insert(index, new_interval)
             inserted = True
             break
+
     if not inserted:
         intervals.append(new_interval)
 
-    merged_intervals = [intervals.pop(0)]
 
-    while intervals:
-        last = merged_intervals.pop()
-        inter = intervals.pop(0)
-
-        if inter[0] <= last[1]:
-            merged = [min(last[0], inter[0]), max(last[1], inter[1])]
-            merged_intervals.append(merged)
-        else:
-            merged_intervals.append(last)
-            merged_intervals.append(inter)
-    
-    return merged_intervals
-        
-
-    
 if __name__ == '__main__':
-    intervals = [[2,6],[7,9]]
-    new_interval = [15,18]
+    intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+    new_interval = [4, 8]
 
-    print(insert(intervals, new_interval))
+    print(merge_inter(intervals, new_interval))
