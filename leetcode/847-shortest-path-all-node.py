731a465ee586f17062fed8aeb4ace235dec99d5e
A283le <alexdominguez169@gmail.com>
2025-06-14 13:43:43 -0400

    The bug causing the issue wasn't even big, found it within seconds. I had flippe dthe cost and start in priority queue without even realizing it.

--- leetcode/847-shortest-path-all-node.py
+++ leetcode/847-shortest-path-all-node.py
@@ -3,18 +3,23 @@ class Solution:
         best = float("inf")
 
         for node in range(len(graph)):
+            print(node)
             best = min(best, self.visitEverything(node, graph))
 
         return best
 
     def visitEverything(self, start: int, graph: List[List[int]]) -> int:
         pq = []
-        heapq.heappush(pq, (start, 0, set([start])))
+        cost = 0
+        visited = set([start])
+
+        heapq.heappush(pq, (cost, start, visited))
 
         while pq:
             cost, node, visited = heapq.heappop(pq)
 
             if len(visited) == len(graph):
+                print(cost)
                 return cost
 
             for connection in graph[node]:
