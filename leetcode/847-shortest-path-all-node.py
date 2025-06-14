68b7e0f1b1c3246c53849a945a3905f12d75f961
A283le <alexdominguez169@gmail.com>
2025-06-14 14:06:52 -0400

    Was not really aware of the bitmask optimization, had to ask chatgpt questions like it was an interviewer so it wouldn't reveal too much information. But this proves to me the original algorithm I selected is correct, just slow.

--- leetcode/847-shortest-path-all-node.py
+++ leetcode/847-shortest-path-all-node.py
@@ -4,26 +4,27 @@ class Solution:
 
         for node in range(len(graph)):
             print(node)
-            best = min(best, self.visitEverything(node, graph))
+            best = min(best, self.visitEverything(node, graph, best))
 
         return best
 
-    def visitEverything(self, start: int, graph: List[List[int]]) -> int:
+    def visitEverything(self, start: int, graph: List[List[int]], best: int) -> int:
         pq = []
         cost = 0
-        visited = set([start])
+        visited = 0
+        filled = (1 << len(graph)) - 1
 
         heapq.heappush(pq, (cost, start, visited))
 
         while pq:
             cost, node, visited = heapq.heappop(pq)
+            visited = visited | (1 << node)
 
-            if len(visited) == len(graph):
-                print(cost)
+            if visited == filled:
+                print(cost, best)
                 return cost
+            if cost >= best:
+                return float("inf")
 
             for connection in graph[node]:
-                vcopy = copy.copy(visited)
-                vcopy.add(connection)
-
-                heapq.heappush(pq, (cost + 1, connection, vcopy))
+                heapq.heappush(pq, (cost + 1, connection, visited))
